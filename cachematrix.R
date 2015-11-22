9adc30fc3f5c60bb564c6d738267cde2d38ba11a
Saba Danawala <sabadanawala86@gmail.com>
2015-11-22 18:08:06 -0500

    RProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -42,12 +42,10 @@ inverse <- y$getInverse()
 if(!is.null(inverse)) {
   message("getting cached data")
   return(inverse)
-  
 # otherwise get the matrix, caclulate the inverse and store it in the cache
 data <- y$getMatrix()
 inverse <- solve(data)
 y$cacheInverse(inverse)
-
 # return the inverse
 inverse
 }
