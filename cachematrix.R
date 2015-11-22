7cf50a67020b5db7ef2f284501d3fd12580b9f05
Saba Danawala <sabadanawala86@gmail.com>
2015-11-22 18:09:05 -0500

    Revert "RProgrammingAssignment2"
    This reverts commit 9adc30fc3f5c60bb564c6d738267cde2d38ba11a.

--- cachematrix.R
+++ cachematrix.R
@@ -42,10 +42,12 @@ inverse <- y$getInverse()
 if(!is.null(inverse)) {
   message("getting cached data")
   return(inverse)
+  
 # otherwise get the matrix, caclulate the inverse and store it in the cache
 data <- y$getMatrix()
 inverse <- solve(data)
 y$cacheInverse(inverse)
+
 # return the inverse
 inverse
 }
